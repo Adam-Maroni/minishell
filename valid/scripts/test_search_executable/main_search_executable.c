6a122f537b3a425ac661ccc783490b239cae1ec5
amaroni <amaroni@student.42.fr>
2022-02-26 19:27:28 +0100

    Update test script of search_executable

--- valid/scripts/test_search_executable/main_search_executable.c
+++ valid/scripts/test_search_executable/main_search_executable.c
@@ -1,32 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_search_executable.c                           :+:      :+:    :+:   */
+/*   main_search_executable.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amaroni <amaroni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 14:43:52 by amaroni           #+#    #+#             */
-/*   Updated: 2022/02/26 15:00:50 by amaroni          ###   ########.fr       */
+/*   Updated: 2022/02/26 18:48:35 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#define VALID 0
+#define INVALID 1
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*cmd;
 	char	*envar_path;
+	int	rt;
 
+	rt = VALID;
 	if (argc != 2 || !argv || !envp)
-		return (1);
+		return (INVALID);
 	envar_path = ft_extract_envar_path(envp);
 	if (!envar_path)
 	{
 		printf("PATH variable not found.\n");
-		return (1);
+		return (INVALID);
 	}
 	cmd = ft_search_executable(argv[1], envar_path);
 	if (!cmd)
-		return (1);
-	return (0);
+		rt = INVALID;
+	free(cmd);
+	return (rt);
 }
--- valid/scripts/test_search_executable/test_search_executable.sh
+++ valid/scripts/test_search_executable/test_search_executable.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 OUTPUT=test_search_executable
+VALID_CODE=0
+INVALID_CODE=1
 
 GREEN_TEXT="\e[92m"
 DEFAULT_TEXT="\e[0m"
 RED_TEXT="\e[31m"
 BOLD_TEXT="\e[1m"
 
+#param1 = cmd
+#param2 = expected returned value
 function test_case {
 	make
-	./$OUTPUT $1
+	valgrind -q ./$OUTPUT $1
 	RETURN_CODE=$?
-	echo -n "$1 => "
-	if [ $RETURN_CODE == 0 ]; then 
+	echo -n "'$1' => "
+	if [ $RETURN_CODE == $2 ]; then 
 		echo -e "$GREEN_TEXT Alright $DEFAULT_TEXT"
 	else
 		echo -e "$RED_TEXT Not found $DEFAULT_TEXT"
@@ -21,13 +25,16 @@ function test_case {
 }
 
 echo
-echo -e "$BOLD_TEXT----------Should Return Alright----------$DEFAULT_TEXT"
-test_case "pwd"
-test_case "echo"
-test_case "ls"
-echo -e "$BOLD_TEXT----------Should Return Not found----------$DEFAULT_TEXT"
-test_case "dfghnjmk,l."
-test_case "123"
-test_case "       "
+echo -e "$BOLD_TEXT----------Valid commands----------$DEFAULT_TEXT"
+test_case "pwd" $VALID_CODE
+test_case "echo" $VALID_CODE
+test_case "ls" $VALID_CODE
+test_case "cat" $VALID_CODE
+test_case "bash" $VALID_CODE
+echo -e "$BOLD_TEXT----------Invalid commands----------$DEFAULT_TEXT"
+test_case "dfghnjmk,l." $INVALID_CODE
+test_case "123" $INVALID_CODE
+test_case "       " $INVALID_CODE
+test_case "" $INVALID_CODE
 echo 
 
