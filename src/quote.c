fa0bb8cf1331d7d607e47a47c17825c4fbc0acce
Kevin JEBANE <kejebane@e2r12p9.clusters.42paris.fr>
2022-04-19 18:59:34 +0200

    NORME quote

--- src/quote.c
+++ src/quote.c
@@ -24,30 +24,29 @@
  * 			(supposedly, the current subcommand).
  * \return	char *, a new string. Alternative version of ARG_0.
  */
- char	*ft_alt_subcommand_quote(char *subcommand, char first)
- //char	*ft_alt_subcommand_quote(char *subcommand)
- {
-	 char	*alt_subcommand;
-	 int		i;
-	 int		quote;
+char	*ft_alt_subcommand_quote(char *subcommand, char first)
+//char	*ft_alt_subcommand_quote(char *subcommand)
+{
+	char	*alt_subcommand;
+	int		i;
+	int		quote;
 
-	 i = 0;
-	 quote = 0;
-	 alt_subcommand = ft_calloc(sizeof(char), ft_strlen(subcommand) + 1);
-	 while (subcommand[i])
-	 {
+	i = 0;
+	quote = 0;
+	alt_subcommand = ft_calloc(sizeof(char), ft_strlen(subcommand) + 1);
+	while (subcommand[i])
+	{
 		if (subcommand[i] == first)
-		//if (subcommand[i] == 34)
 			quote++;
 		if (quote == 1 && subcommand[i] == 32)
 			alt_subcommand[i] = 127;
 		else
 			alt_subcommand[i] = subcommand[i];
 		i++;
-	 }
-	 alt_subcommand[i] = '\0';
-	 return (alt_subcommand);
- }
+	}
+	alt_subcommand[i] = '\0';
+	return (alt_subcommand);
+}
 
 /**
  * \fn	char    *ft_recover_string(char *str)
@@ -59,18 +58,14 @@
  * \return	char *str, the string we worked on.
  */
 char	*ft_recover_string(char *str, char first, int keep)
-//char	*ft_recover_string(char *str, char first)
-//char	*ft_recover_string(char *str)
 {
-	int	i;
-	int	y;
+	int		i;
+	int		y;
 	char	*recovered_str;
-
 
 	i = 0;
 	y = 0;
 	if (ft_strchr(str, first) == NULL)
-	//if (ft_strchr(str, 34) == NULL)
 		return (str);
 	recovered_str = ft_calloc(ft_strlen(str) + 1, sizeof(char));
 	while (str[i])
@@ -78,7 +73,6 @@ char	*ft_recover_string(char *str, char first, int keep)
 		if (str[i] == 127)
 			recovered_str[y++] = 32;
 		else if (str[i] != first && keep == -1)
-	//	else if (str[i] != first)
 			recovered_str[y++] = str[i];
 		else if (keep == 1)
 			recovered_str[y++] = str[i];
@@ -88,7 +82,6 @@ char	*ft_recover_string(char *str, char first, int keep)
 	str = ft_strdup(recovered_str);
 	return (str);
 }
-
 
 /**
  * \fn	char   **ft_recover_word_array(char **word_array)
@@ -100,13 +93,9 @@ char	*ft_recover_string(char *str, char first, int keep)
  * \param	char **word_array, the word_array to work on.
  * \return	char **word_array, the word_array we worked on.
  */
- char	**ft_recover_word_array(char **word_array, int keep)
- //char	**ft_recover_word_array(char **word_array)
+char	**ft_recover_word_array(char **word_array, int keep)
 {
-//	char	*recovered_word;
 	int		i;
-//	int		x;
-//	int		z;
 
 	i = 0;
 	while (word_array[i])
@@ -115,23 +104,7 @@ char	*ft_recover_string(char *str, char first, int keep)
 			|| ft_strchr(word_array[i], 39) != NULL )
 		{
 			ft_which_recover(word_array[i], keep);
-		//	ft_which_recover(word_array[i]);
-		//	ft_recover_string(word_array[i]);
-	/*		x = 0;
-			z = 0;
-			recovered_word = (char *)ft_calloc(ft_strlen(word_array[i]) + 1, sizeof(char));
-			while (word_array[i][x])
-			{
-				if (word_array[i][x] == 127)
-					recovered_word[z++] = 32;
-				else if (word_array[i][x] != 34)
-					recovered_word[z++] = word_array[i][x];
-				x++;
-			}
-			free(word_array[i]);
-			word_array[i] = ft_strdup(recovered_word);
-			free(recovered_word);
-	*/	}
+		}
 		i++;
 	}
 	return (word_array);
@@ -145,23 +118,19 @@ char	*ft_recover_string(char *str, char first, int keep)
  */
 char	*ft_which_alt(char *str)
 {
-	int	d_quote;
-	int	s_quote;
+	int		d_quote;
+	int		s_quote;
 	char	*alt_str;
 
 	d_quote = ft_position(str, 34);
 	s_quote = ft_position(str, 39);
-//	if (s_quote == -1 && d_quote == -1)
-	//
 	if ((s_quote >= 0 && d_quote == -1)
 		|| (s_quote < d_quote
-		&& s_quote >= 0 && d_quote >= 0))
-	//
+			&& s_quote >= 0 && d_quote >= 0))
 		alt_str = ft_alt_subcommand_quote(str, 39);
 	else if ((d_quote >= 0 && s_quote == -1)
 		|| (d_quote < s_quote
-		&& s_quote >= 0 && d_quote >= 0))
-	//
+			&& s_quote >= 0 && d_quote >= 0))
 		alt_str = ft_alt_subcommand_quote(str, 34);
 	else
 		alt_str = ft_strdup(str);
@@ -184,15 +153,11 @@ int	ft_which_recover(char *str, int keep)
 	s_quote = ft_position(str, 39);
 	if ((s_quote >= 0 && d_quote == -1)
 		|| (s_quote < d_quote
-		&& s_quote >= 0 && d_quote >= 0))
-//	if (s_quote < d_quote)
+			&& s_quote >= 0 && d_quote >= 0))
 		ft_recover_string(str, 39, keep);
-		//ft_recover_string(str, 39);
 	else if ((d_quote >= 0 && s_quote == -1)
 		|| (d_quote < s_quote
-		&& s_quote >= 0 && d_quote >= 0))
-//	else if (d_quote < s_quote)
+			&& s_quote >= 0 && d_quote >= 0))
 		ft_recover_string(str, 34, keep);
-		//ft_recover_string(str, 34);
 	return (0);
 }
