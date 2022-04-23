f10e1f2482846810ae6471a8cb1527fd923bde75
amaroni <amaroni@student.42.fr>
2022-04-23 15:17:29 +0200

    We use autoexpect script.exp to handle interractive interraction with valgrind.

--- valid/scripts/memleaks_tracking/memleaks_tracking.sh
+++ valid/scripts/memleaks_tracking/memleaks_tracking.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-#1 Should run a make valgrind
-#2 Should insert inside ./minishell the test_case in interactive mode
+MINISHELL_PATH="../../.."
+EXECUTABLE_NAME="$MINISHELL_PATH/minishell"
+PROMPT_STRING="Minishell>"
+
+# arg1 = TEST_NUMBER , arg2 = PROMPT_STRING , arg3 = EXECUTABLE_NAME , arg4 = COMMAND
+test_case(){
+	(cd $MINISHELL_PATH && make)
+	./script.exp "$1" $PROMPT_STRING $EXECUTABLE_NAME "$2"
+}
+
+main(){
+	test_case "0" "echo a"
+}
+
+
+main
 
 
 
 
 
-../../../minishell
-expect "Minishell>"
-send "echo a"
