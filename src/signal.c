064830cc119c5d0d1126669753e16ce270f2ba2d
amaroni <amaroni@student.42.fr>
2022-05-02 18:29:08 +0200

    Ctrl-C empty the prompt

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 16:59:16 by amaroni           #+#    #+#             */
-/*   Updated: 2022/04/27 13:16:08 by amaroni          ###   ########.fr       */
+/*   Updated: 2022/05/02 18:27:28 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	ft_sigint_handler(int signum)
 	if (signum == 3)
 		return ;
 	write(STDIN_FILENO, &character , sizeof(char));
+	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
